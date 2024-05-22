bf9a79a98945c398c49847fd87a73752251b8921
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-22 13:31:17 +0330

    create base ui app

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,12 @@
-import { useState } from "react";
 import "./App.css";
-import AddNewNote from "./components/AddNewNote";
-import NoteList from "./components/NoteList";
-import NoteStatus from "./components/NoteStatus";
-import NoteHeader from "./components/NoteHeader";
-
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [sortBy, setSortBy] = useState("latest");
-
-  const hanldeAddNote = (newNote) => {
-    setNotes((prevNotes) => [...prevNotes, newNote]);
-  };
-
-  const handleDeleteNote = (id) => {
-    // const filteredNotes = notes.filter((n) => n.id !== id);
-    // setNotes(filteredNotes);
-    setNotes((prevNotes) => prevNotes.filter((n) => n.id !== id));
-  };
-
-  const handleCompleteNote = (e) => {
-    const noteId = Number(e.target.value);
-    // const newNotes = notes.map((note) =>
-    //   note.id === noteId ? { ...note, completed: !note.completed } : note
-    // );
-    // setNotes(newNotes);
-
-    setNotes((prevNotes) =>
-      prevNotes.map((note) =>
-        note.id === noteId ? { ...note, completed: !note.completed } : note
-      )
-    );
-  };
-
-
-
-  return (
-    <div className="container">
-      <NoteHeader
-        notes={notes}
-        sortBy={sortBy}
-        onSort={(e) => setSortBy(e.target.value)}
-      />
-      <div className="note-app">
-        <AddNewNote onAddNote={hanldeAddNote} />
-        <div className="note-container">
-          <NoteStatus notes={notes} />
-          <NoteList
-            notes={notes}
-            sortBy={sortBy}
-            onDelete={handleDeleteNote}
-            onComplete={handleCompleteNote}
-          />
-        </div>
-      </div>
+  return <div className="container">
+    <div className="note-header">note header</div>
+    <div className="note-app">
+      <div className="add-new-note">add new note form</div>
+      <div className="note-container">notes</div>
     </div>
-  );
+  </div>
 }
 
 export default App;
