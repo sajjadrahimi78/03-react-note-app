87361829fd964b9323882274f8d5194ccdbf2b36
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-23 19:00:44 +0330

    add handleDeleteNote for deleted note item and filterd note items

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,31 @@ import AddNewNote from "./components/AddNewNote";
 import NoteList from "./components/NoteList";
 import { useState } from "react";
 
-
 function App() {
   // states
   const [notes, setNotes] = useState([]);
+
+  // functions
   const handleAddNote = (newNote) => {
-    setNotes(prevNotes => [...prevNotes , newNote])
-  }
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
+
+  const handleDeleteNote = (id) => {
+    // first way -> use filter method to unmutate array
+    // const filteredNotes = notes.filter((n) => n.id !== id);
+    // setNotes(filteredNotes);
+
+    // callback way
+    setNotes(privNotes => privNotes.filter(n => n.id !== id))
+  };
 
   return (
     <div className="container">
       <div className="note-header">note header</div>
       <div className="note-app">
-        <AddNewNote onAddNote={handleAddNote}/>
+        <AddNewNote onAddNote={handleAddNote} />
         <div className="note-container">
-          <NoteList notes={notes}/>
+          <NoteList notes={notes} onDelete={handleDeleteNote} />
         </div>
       </div>
     </div>
