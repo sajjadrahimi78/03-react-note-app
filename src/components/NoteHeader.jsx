ff23032292d78000897ff74960b2de292aa636fa
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-05-25 19:25:19 +0330

    create NoteHeader component and completed

--- src/components/NoteHeader.jsx
+++ src/components/NoteHeader.jsx
@@ -1,9 +1,10 @@
 
-function NoteHeader({ notes, sortBy, onSort }) {
+function NoteHeader({ notes, sortBy , onSort}) {
+  const allNotes = notes.length;
 
   return (
     <div className="note-header">
-      <h1>My Notes ({notes.length})</h1>
+      <h1>My Notes ({allNotes})</h1>
       <select value={sortBy} onChange={onSort}>
         <option value="latest">Sort based on latest notes</option>
         <option value="earliest">Sort based on earliest notes</option>
