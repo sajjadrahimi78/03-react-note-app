1ee7ec85c911c7d3dfc71544ba1885908f40944c
Sajjad Rahimi <sajjad021.5555@gmail.com>
2024-06-04 14:24:37 +0330

    add useReducer to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
 import "./App.css";
 import AddNewNote from "./components/AddNewNote";
 import NoteList from "./components/NoteList";
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import NoteStatus from "./components/NoteStatus";
 import NoteHeader from "./components/NoteHeader";
 
+function notesReducer(state, { type, payload }) {
+  switch (type) {
+    case "add":
+      return [...state, payload];
+      break;
+    case "delete":
+      return state.filter((s) => s.id !== payload);
+      break;
+    case "complete":
+      return state.map((note) =>
+        note.id === payload ? { ...note, completed: !note.completed } : note
+      );
+      break;
+    default:
+      throw new Error("Unknown Error" + type);
+      break;
+  }
+}
+
 function App() {
   // states
-  const [notes, setNotes] = useState([]);
+  // const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(notesReducer, []);
   const [sortBy, setSortBy] = useState("latest");
 
   // functions
   const handleAddNote = (newNote) => {
-    setNotes((prevNotes) => [...prevNotes, newNote]);
+    // setNotes((prevNotes) => [...prevNotes, newNote]);
+    dispatch({ type: "add", payload: newNote });
   };
 
   const handleDeleteNote = (id) => {
@@ -21,7 +42,8 @@ function App() {
     // setNotes(filteredNotes);
 
     // callback way
-    setNotes((privNotes) => privNotes.filter((n) => n.id !== id));
+    // setNotes((privNotes) => privNotes.filter((n) => n.id !== id));
+    dispatch({ type: "delete", payload: id });
   };
 
   const handleCompleteNote = (e) => {
@@ -33,14 +55,13 @@ function App() {
     // setNotes(newNotes);
 
     // callback way
-    setNotes((privNotes) =>
-      privNotes.map((note) =>
-        note.id === noteId ? { ...note, completed: !note.completed } : note
-      )
-    );
+    // setNotes((privNotes) =>
+    //   privNotes.map((note) =>
+    //     note.id === noteId ? { ...note, completed: !note.completed } : note
+    //   )
+    // );
+    dispatch({ type: "complete", payload: noteId });
   };
-
-  
 
   return (
     <div className="container">
@@ -52,7 +73,7 @@ function App() {
       <div className="note-app">
         <AddNewNote onAddNote={handleAddNote} />
         <div className="note-container">
-          <NoteStatus notes={notes}/>
+          <NoteStatus notes={notes} />
           <NoteList
             notes={notes}
             sortBy={sortBy}
